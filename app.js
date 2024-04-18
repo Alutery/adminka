d5dbe54f90a0be2e2059d49ed5708510637e5c75
Алия Сафина <a.safina@sbereducation.ru>
2024-04-18 19:39:53 +0300

    Добавлен фронтенд в папку public

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
 const app = express();
 
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  fs.readFile("./public/index.html", "utf-8").then((data) => {
+    res.header("Content-Type", "text/html");
+    res.send(data);
+  });
 });
 
 app.listen(PORT, () => {
